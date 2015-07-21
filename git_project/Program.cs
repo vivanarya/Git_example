41a683f435f27deae7eb0b489bff764544cf6fc1
vasant rana <rbasant006@gmail.com>
2015-07-21 07:59:07 -0700

    This is my change

---
+++ git_project/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace git_project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //this is my Git project
+        }
+    }
+}
